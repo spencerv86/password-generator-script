5584d7adf28885de7c492dad6402a968f3d96dd2
Spencer Vaughan <spencer.vaughan@gmail.com>
2020-12-09 18:44:06 -0500

    added basic 'prompts' and 'confirms' to the generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,11 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 
-var upperCaseChar = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var lowerCaseChar = ["abcdefghijklmnopqrstuvwxyz"];
-var numericChar = ["1234567890"];
-var specialChar = ["~!@#$%^&*()-_=+<>?"];
+
+var upperChar = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowerChar = ["abcdefghijklmnopqrstuvwxyz"];
+var numChar = ["1234567890"];
+var specChar = ["~!@#$%^&*()-_=+<>?"];
 
 
 function writePassword() {
@@ -18,13 +19,18 @@ function writePassword() {
 
 
 function generatePassword() {
+  var pwLength = prompt(
+    "How long would you like your password to be? (Please enter a number between 8-128)");
+  var upperQ = confirm("Would you like to add uppercase letters to your password?");
+  var lowerQ = confirm("Would you like to add lowercase letters to your password?");
+  var numQ = confirm("Would you like to add numeric characters to your password? (Ex: '1', '2', '7')");
+  var specQ = confirm("Would you like to add special characters to your password? (Ex: '?', '@', '$')");
 
-
-
+  
   return "temporary password";
 }
 
-console.log()
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
